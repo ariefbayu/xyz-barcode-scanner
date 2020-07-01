24ca5e3ea6933409c570a6a8428e88dc17b1c20e
Arief Bayu Purwanto <ariefbayu@gmail.com>
2020-07-01 11:03:25 +0700

    initial add for continuous scan activity

---
+++ app/src/main/java/xyz/ariefbayu/xyzbarcodescanner/ContinuousActivity.kt
@@ -0,0 +1,117 @@
+package xyz.ariefbayu.xyzbarcodescanner
+
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v4.content.ContextCompat
+import android.view.View
+import com.google.zxing.ResultPoint
+import com.google.zxing.client.android.BeepManager
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.CaptureManager
+import java.util.*
+import kotlinx.android.synthetic.main.activity_continuous.*
+import kotlinx.android.synthetic.main.activity_continuous.barcodeView
+import kotlinx.android.synthetic.main.activity_continuous.btnScanContinuous
+import kotlinx.android.synthetic.main.activity_continuous.txtResultContinuous
+
+class ContinuousActivity : AppCompatActivity() {
+    lateinit var captureManager: CaptureManager
+    var torchState: Boolean = false
+
+    var scanContinuousState: Boolean = false
+    lateinit var scanContinuousBG: Drawable
+    lateinit var beepManager: BeepManager
+    lateinit var lastScan: Date
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_continuous)
+
+        title = "Continuous Scan"
+
+        captureManager = CaptureManager(this, barcodeView)
+        captureManager.initializeFromIntent(intent, savedInstanceState)
+
+        beepManager = BeepManager(this)
+        beepManager.isVibrateEnabled = true
+
+        scanContinuousBG = btnScanContinuous.background
+        lastScan = Date()
+
+        var callback = object : BarcodeCallback {
+            override fun barcodeResult(result: BarcodeResult?) {
+                result?.let {
+                    val current = Date()
+                    val diff = current.time - lastScan.time
+                    if(diff >= 1000){
+                        txtResultContinuous.text = it.text
+                        lastScan = current
+                        beepManager.playBeepSoundAndVibrate()
+
+                        animateBackground()
+                    }
+                }
+            }
+
+            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) {
+            }
+        }
+
+        btnScanContinuous.setOnClickListener(View.OnClickListener {
+            if(!scanContinuousState){
+                scanContinuousState = !scanContinuousState
+                btnScanContinuous.setBackgroundColor(ContextCompat.getColor(InlineScanActivity@this, R.color.colorPrimary))
+                txtResultContinuous.text = "scanning..."
+                barcodeView.decodeContinuous(callback)
+            } else {
+                scanContinuousState = !scanContinuousState
+                btnScanContinuous.background = scanContinuousBG
+                barcodeView.barcodeView.stopDecoding()
+            }
+        })
+
+        btnTorch.setOnClickListener {
+            if(torchState){
+                torchState = false
+                barcodeView.setTorchOff()
+            } else {
+                torchState = true
+                barcodeView.setTorchOn()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        captureManager.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        captureManager.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        captureManager.onDestroy()
+    }
+
+    private fun animateBackground(){
+        val colorFrom = resources.getColor(R.color.colorAccent)
+        val colorTo = resources.getColor(R.color.colorPrimary)
+        val colorAnimation =
+            ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo)
+        colorAnimation.duration = 250 // milliseconds
+
+        colorAnimation.addUpdateListener { animator -> txtResultContinuous.setBackgroundColor(animator.animatedValue as Int) }
+        colorAnimation.start()
+    }
+}
